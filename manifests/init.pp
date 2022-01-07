236d32ba50dce873e88e7842a8fbf19144412c18
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-07 15:17:09 +0800

    Corrected syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,24 +11,25 @@ class review (
 
     $users = lookup('users')
     $users.each |String $user| {
-    if $facts['kernel'] == 'Linux' {
-      $homedir = $user ? {
-        'root'  => '/root',
-        default => "/home/${user}",
-      }
+      if $facts['kernel'] == 'Linux' {
+        $homedir = $user ? {
+          'root'  => '/root',
+          default => "/home/${user}",
+        }
 
-      user { $user:
-        ensure     => present,
-        shell      => '/bin/bash',
-        managehome => true,
-      }
+        user { $user:
+          ensure     => present,
+          shell      => '/bin/bash',
+          managehome => true,
+        }
 
-      file { "${homedir}/.bashrc":
-        ensure => file,
-        owner  => $user,
-        group  => $user,
-        mode   => '0644',
-        source => 'puppet:///modules/review/bashrc',
+        file { "${homedir}/.bashrc":
+          ensure => file,
+          owner  => $user,
+          group  => $user,
+          mode   => '0644',
+          source => 'puppet:///modules/review/bashrc',
+        }
       }
     }
   }
