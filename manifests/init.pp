093ab0f660e83c26da90ae725cd484a40b4f293d
justinkhwong75 <justinkhwong75@gmail.com>
2022-01-08 16:54:25 +0800

    Added user hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,71 +5,73 @@
 # @example
 #   include review
 class review (
-  $users = lookup('review::users', Array[String], { 'merge' => 'unique' }, 'review')
-  $users_hash = lookup('review::users_hash', { 'merge' => 'unique' }, {}})
+  #$users = lookup('review::users', Array[String], { 'merge' => 'unique' }, 'review')
+  $users_hash = lookup('review::users_hash', Hash[String], { 'merge' => 'unique' }, {})
+
 ) {
     case $facts['kernel'] {
       'Linux': {
 
         include review::motd
 
-        $users.each |String $user| {
-          $homedir = $user ? {
-            'root'  => '/root',
-            default => "/home/${user}",
-          }
+        #$users.each |String $user| {
+        #  $homedir = $user ? {
+        #    'root'  => '/root',
+        #    default => "/home/${user}",
+        #  }
 
-          user { $user:
-            ensure     => present,
-            shell      => '/bin/bash',
-            managehome => true,
-          }
+        # user { $user:
+          #  ensure     => present,
+          # shell      => '/bin/bash',
+          #  #managehome => true,
+          #}
 
-          file { "${homedir}/.bashrc":
-            ensure => file,
-            owner  => $user,
-            group  => $user,
-            mode   => '0644',
-            source => 'puppet:///modules/review/bashrc',
-          }
+          #file { "${homedir}/.bashrc":
+          #  ensure => file,
+          #  owner  => $user,
+          #  group  => $user,
+          #  mode   => '0644',
+          #  source => 'puppet:///modules/review/bashrc',
+          #}
 
-          file { "${homedir}/yay.txt":
-            ensure  => file,
-            owner   => $user,
-            group   => $user,
-            content => 'I love Puppet!',
-          }
-        }
-        $users_hash.each | String $usr, Hash $options {
-          $homedir = $usr ? {
-            'root'  => '/root',
-            default => "/home/${usr}",
-          }
-          user { $usr:
-            ensure     => present,
-            shell      => $options[shell],
-            managehome => true,
-          }
-          file { "${homedir}/yay.txt":
-            ensure  => file,
-            owner   => $usr,
-            group   => $usr,
-            content => $options[filecontent],
+          #file { "${homedir}/yay.txt":
+          #  ensure  => file,
+          #  owner   => $user,
+          #  group   => $user,
+          #  content => 'I love Puppet!',
+          #}
+
+          $users_hash.each | String $usr, Hash $options | {
+            $homedir = $usr ? {
+              'root'  => '/root',
+              default => "/home/${usr}",
+            }
+            user { $usr:
+              ensure     => present,
+              shell      => $options[shell],
+              managehome => true,
+            }
+            file { "${homedir}/yay.txt":
+              ensure  => file,
+              owner   => $usr,
+              group   => $usr,
+              content => $options[filecontent],
+            }
           }
         }
       }
       'windows': {
-        $users.each |String $user| {
+        #$users.each |String $user| {
 
-          $homedir = "C:\\Users\\$user"
+        #  $homedir = "C:\\Users\\$user"
 
-          user { $user:
-            ensure => present,
-            name   => $user,
-            groups => [ 'Users' ],
-          }
-        }
+        #  user { $user:
+        #    ensure => present,
+        #    name   => $user,
+        #    groups => [ 'Users' ],
+        #  }
+        #}
       }
       default: { notify { "Too bad, so sad!\n": } }
     }
-}
+  }
